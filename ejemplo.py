f413495158c609659ebbd46a8c94756799faf8f7
JimenaMaSuMu <jimenaszmz@gmail.com>
2024-11-28 21:22:29 -0500

    parte 2

--- ejemplo.py
+++ ejemplo.py
@@ -11,3 +11,13 @@ cv2.destroyAllWindows()
 
 #para guardar una imagen
 cv2.imwrite('imagenguardar.jpg',img)
+
+##Gray Images-----
+img_gray = cv2.imread('imagen_1.jpg', cv2.IMREAD_GRAYSCALE)
+# Es lo mismo | img_gray = cv2.imread('imagen_1.jpg', 0)
+cv2.imshow("ventana2",img_gray)
+cv2.waitKey()
+cv2.destroyAllWindows()
+cv2.imwrite('imagenguardargris.jpg',img_gray)
+
+##waitKey()
